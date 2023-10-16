7cdcf61e3fb6579c444709596c2ede77b4e63e91
haunguyendev <haunt150603@gmail.com>
2023-10-16 08:57:13 +0700

    update getf() to use recersion and regression tessting. See green4ver

--- src/main/java/com/hau/math/core/MathUtility.java
+++ src/main/java/com/hau/math/core/MathUtility.java
@@ -17,16 +17,47 @@ public class MathUtility {
     //0! = 1! quy ước 0! = 1
     //vì giai thừa tăng rất nhanh, 21! kiể long chứa không nổi(vượt 18 số 0)
     //do dó hàm getF() của chúng ta chỉ chơi với n 0..20
-    public static long getFactorial(int n){
+//    public static long getFactorial(int n){
+//        if(n < 0 || n > 20)
+//            throw new IllegalArgumentException("Invalid n. n must be 0..20");
+//        if(n == 0 || n == 1)
+//            return 1;
+//        long result = 1; //sống sót đến lệnh này , sure n= 2..20
+//        for (int i = 2; i <= n; i++) 
+//            result *= i;
+//        
+//        return result;
+//    }
+    
+       public static long getFactorial(int n){
         if(n < 0 || n > 20)
             throw new IllegalArgumentException("Invalid n. n must be 0..20");
         if(n == 0 || n == 1)
             return 1;
-        long result = 10; //sống sót đến lệnh này , sure n= 2..20
-        for (int i = 2; i <= n; i++) 
-            result *= i;
         
-        return result;
+        return n*getFactorial(n-1);
+        // tối ưu code, thau ruột của cái hàm để chạy ngon hơn
+        //Nhanh hơn, hoặc code viết đẹp hơn
+        //hoặc fix bug thì phải sửa code
+        // DÙ BẠN SỬA CODE VỚI LÍ DO GÌ THÌ CODE PHẢI ĐƯỢC TEST LẠI
+        // VÀ TEST LẠI ĐỂ ĐẢM BẢO RẰNG NÓ VẪN XANH NHƯ LẦN TRƯỚC 
+        // NẾU TA VIẾT CODE ĐI KÈM THEO TEST SCRIPT THÌ VIỆC TEST LẠI CÁI
+        // HÀM SẼ CỰC NHANH, KHÔNG CẦN DÙNG MẮT ĐỂ DÒ TỪNG DÒNG LỆNH ĐÚNG
+        // SAI, MÀ CHỈ CẦN NHÌN MÀU XANH LÀ ĐỦ - VÌ HÀM TRẢ VỀ NHƯ EXPECTED
+        //                                             Kiểm thử hồi quy
+        // Kiểm thử lại hàm cực nhanh, vì github actions lo giùm
+        // Ta chỉ cần màu xanh, hoặc nhận email chửi vì code đỏ
+        
+        //5!=1.2.3.4! X5
+        //..
+        //..
+        //..
+        
+        
+        
     }
+    
+    
+    
     
 }
